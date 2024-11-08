ba6b80604b77f00b1da97ee0f8dc09481bf6f798
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-11-08 19:05:27 +0530

    added code

--- main.js
+++ main.js
@@ -1,2 +1,66 @@
 import "./style.css";
 import "./src/theme";
+
+const tasks = [
+  {
+    id: "1",
+    task: "Problem Solving",
+    isComplete: false,
+  },
+  {
+    id: "2",
+    task: "Project",
+    isComplete: false,
+  },
+  {
+    id: "3",
+    task: "Learn",
+    isComplete: false,
+  },
+  {
+    id: "4",
+    task: "Side Project",
+    isComplete: false,
+  },
+  {
+    id: "5",
+    task: "Programming Language",
+    isComplete: false,
+  },
+  {
+    id: "6",
+    task: "Blog",
+    isComplete: false,
+  },
+  {
+    id: "7",
+    task: "Apply",
+    isComplete: false,
+  },
+  {
+    id: "8",
+    task: "Video",
+    isComplete: false,
+  },
+];
+
+let taskCards;
+
+const tasksSection = document.getElementById("tasks");
+
+function setTaskCards() {
+  taskCards = tasks
+    .map((task) => {
+      return `<section class="card" id="${task.id}">
+            <article class="flex items-center gap-2">
+              <input class="checkbox" type="checkbox" name="" value="" />
+                <p>${task.task}</p>
+            </article>
+          </section>`;
+    })
+    .join("");
+
+  tasksSection.innerHTML = taskCards;
+}
+
+setTaskCards();
